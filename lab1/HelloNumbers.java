363ade68208ee97fb057c05568fde6795c3d1046
Tadori-izanai <u17814@outlook.com>
2022-03-04 19:11:13 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class HelloNumbers {
+    public static void main(String[] args) {
+        for (int i = 0, sum = 0; i < 10; i++) {
+            sum += i;
+            System.out.print(sum + " ");    // 写为 ' ' 则不一样
+        }
+        System.out.println();
+    }
+}
