20a46a867cc37c578c3b5316329f65447ad56831
Tadori-izanai <u17814@outlook.com>
2022-03-18 21:13:03 +0800

    Modified the style for Project 1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,7 +2,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-//    /*
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
@@ -17,8 +16,4 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('e', 'e'));
     }
 
-
-
-
-//    Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
 }
