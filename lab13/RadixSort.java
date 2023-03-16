20fa41216a136d2ac2170146be79ff6d94029e5c
Tadori-izanai <u17814@outlook.com>
2023-03-16 09:14:38 +0800

    lab13: implemented the LSD sort

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -17,7 +17,34 @@ public class RadixSort {
      */
     public static String[] sort(String[] asciis) {
         // TODO: Implement LSD Sort
-        return null;
+        int N = asciis.length;
+        String[] results = new String[N];
+        System.arraycopy(asciis, 0, results, 0, N);
+
+        int maxLength = 0;
+        for (String s : asciis) {
+            if (s.length() > maxLength) {
+                maxLength = s.length();
+            }
+        }
+
+        for (int i = 0; i < N; i += 1) {
+            StringBuilder sb = new StringBuilder(results[i]);
+            for (int k = 0; k < maxLength - results[i].length(); k += 1) {
+                sb.append('\u0000');
+            }
+            results[i] = sb.toString();
+        }
+
+        for (int i = maxLength - 1; i >= 0; i -= 1) {
+            sortHelperLSD(results, i);
+        }
+
+        for (int i = 0; i < N; i += 1) {
+            results[i] = results[i].trim();
+        }
+
+        return results;
     }
 
     /**
@@ -28,7 +55,27 @@ public class RadixSort {
      */
     private static void sortHelperLSD(String[] asciis, int index) {
         // Optional LSD helper method for required LSD radix sort
-        return;
+        int R = 256;
+        int[] count = new int[R];
+        int[] starts = new int[R];
+
+        String[] asciisCopy = new String[asciis.length];
+        System.arraycopy(asciis, 0, asciisCopy, 0, asciis.length);
+
+        for (String s : asciisCopy) {
+            count[(int)(s.charAt(index))] += 1;
+        }
+        int ind = 0;
+        for (int i = 0; i < R; i += 1) {
+            starts[i] = ind;
+            ind += count[i];
+        }
+
+        for (String s : asciisCopy) {
+            int k = (int)(s.charAt(index));
+            asciis[starts[k]] = s;
+            starts[k] += 1;
+        }
     }
 
     /**
@@ -43,6 +90,59 @@ public class RadixSort {
      **/
     private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
         // Optional MSD helper method for optional MSD radix sort
-        return;
+
+        // first, swap those who are shorter than index+1 to the front
+        int numShort = 0;
+        for (int i = start; i < end; i += 1) {
+            if (asciis[i].length() - 1 < index) {
+                String tmp = asciis[i];
+                asciis[i] = asciis[start + numShort];
+                asciis[start + numShort] = tmp;
+                numShort += 1;
+            }
+        }
+        start += numShort;
+        if (start == end) {
+            return;
+        }
+
+        int n = end - start;
+        int R = 256;
+        int[] cnt = new int[R];
+        int[] starts = new int[R];
+
+        String[] asciisCopy = new String[n];
+        System.arraycopy(asciis, start, asciisCopy, 0, n);
+
+        for (String s : asciisCopy) {
+            cnt[(int)(s.charAt(index))] += 1;
+        }
+        for (int i = 0, ind = 0; i < R; i += 1) {
+            starts[i] = ind;
+            ind += cnt[i];
+        }
+
+        for (String s : asciisCopy) {
+            int k = (int)(s.charAt(index));
+            asciis[start + starts[k]] = s;
+            cnt[k] += 1;
+        }
+
+        // do sorting in each bucket
+        for (int i = 0, ind = 0; i < R; i += 1) {
+            if (cnt[i] > 1) {
+                sortHelperMSD(asciis, start + ind, start + ind + cnt[i], index + 1);
+            }
+            ind += cnt[i];
+        }
+    }
+
+    public static String[] sortMSD(String[] asciis) {
+        int N = asciis.length;
+        String[] results = new String[N];
+        System.arraycopy(asciis, 0, results, 0, N);
+
+        sortHelperMSD(results, 0, N, 0);
+        return results;
     }
 }
---
+++ lab13/RadixSortTest.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RadixSortTest {
+    public static void main(String[] args) {
+        String[] touhou = new String[] {
+                "Leimu",
+                "Marisa",
+                "Cirno",
+                "Lumia",
+                "Suwako",
+                "Sanae",
+                "Koishi",
+                "Satori",
+                "Yokari",
+                "Chen"
+        };
+
+        System.out.println(Arrays.toString(touhou));
+
+        String[] touhouSorted = RadixSort.sort(touhou);
+        System.out.println(Arrays.toString(touhouSorted));
+
+        touhouSorted = RadixSort.sortMSD(touhou);
+        System.out.println(Arrays.toString(touhouSorted));
+    }
+
+    @Test
+    public void testSortLSD() {
+        String[] touhou = new String[] {
+                "Leimu",
+                "Marisa",
+                "Cirno",
+                "Lumia",
+                "Suwako",
+                "Sanae",
+                "Koishi",
+                "Satori",
+                "Yokari",
+                "Chen"
+        };
+
+        String[] touhouSorted = RadixSort.sort(touhou);
+        Arrays.sort(touhou);
+        assertArrayEquals(touhouSorted, touhou);
+    }
+
+    @Test
+    public void testSortMSD() {
+        String[] touhou = new String[] {
+                "Leimu",
+                "Marisa",
+                "Cirno",
+                "Lumia",
+                "Suwako",
+                "Sanae",
+                "Koishi",
+                "Satori",
+                "Yokari",
+                "Chen"
+        };
+
+        String[] touhouSorted = RadixSort.sortMSD(touhou);
+        Arrays.sort(touhou);
+        assertArrayEquals(touhouSorted, touhou);
+    }
+}
